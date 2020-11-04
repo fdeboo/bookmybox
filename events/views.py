91bfc371d6fb8a5c568dbd33b1dfd0dce68ea205
Frances <frances.deboo@gmail.com>
2020-11-04 16:22:06 +0000

    Search and Filter functionality applied to events

--- events/views.py
+++ events/views.py
@@ -1,26 +1,37 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from django.conf import settings
-from datetime import datetime
-from .models import Event
+from .models import Event, Category
 
 
 def all_events(request):
     """ A view to show all events, including sorting and search queries """
-    template = 'events/events.html'
+    template = "events/events.html"
     events = Event.objects.all()
+    query = None
+    categories = None
 
     if request.GET:
-        if 'q' in request.GET:
-            query = request.GET['q']
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            events = events.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+        if "q" in request.GET:
+            query = request.GET["q"]
             if not query:
                 messages.error(
                     request, "You didn't enter any search criteria!"
-                    )
-                return redirect(reverse('events'))
+                )
+                return redirect(reverse("events"))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query
+                )
+            events = events.filter(queries)
     context = {
         "events": events,
+        "search_term": query,
+        "current_categories": categories,
     }
 
     return render(request, template, context)
@@ -30,7 +41,7 @@ def event_detail(request, event_id):
     """ A view to show individual event details """
     event = get_object_or_404(Event, pk=event_id)
 
-    template = 'events/event_detail.html'
+    template = "events/event_detail.html"
     context = {
         "event": event,
     }
