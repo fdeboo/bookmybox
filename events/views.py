95b1c82a45754374fb5d34f8d37b6e4a4b2ba027
Frances <frances.deboo@gmail.com>
2020-11-04 22:14:48 +0000

    Sorting links and functionality applied to events

--- events/views.py
+++ events/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Event, Category
 
 
@@ -10,8 +11,25 @@ def all_events(request):
     events = Event.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                """ Annotates each object in the QuerySet with the provided
+                list of query expressions."""
+                events = events.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+                events = events.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             events = events.filter(category__name__in=categories)
@@ -28,10 +46,14 @@ def all_events(request):
                 description__icontains=query
                 )
             events = events.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         "events": events,
         "search_term": query,
         "current_categories": categories,
+        "current_sorting": current_sorting,
     }
 
     return render(request, template, context)
